e13f5853ef907cda97b6e630589be9691fd71e0d
johnand4 <63374727+johnand4@users.noreply.github.com>
2021-05-28 17:53:53 -0700

    Implemented printing "Fizz"
    The function now prints "Fizz" every multiple of 3 rather than the number.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,9 @@
 def fizzbuzz():
-	array = range(1,100)
+	array = []
+	for x in range(1,101): 
+		num = x
+		if (num%3 == 0):
+			num = "Fizz"
+		array = array + [num]
 	for x in array: print(x)
 	return array
