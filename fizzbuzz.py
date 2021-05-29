27df5eadbe02c4368fe47350121d590c0b8febc8
johnand4 <63374727+johnand4@users.noreply.github.com>
2021-05-28 18:48:36 -0700

    Implemented printing "FizzBuzz"
    The function now prints "FizzBuzz" at element 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,10 @@
 def fizzbuzz():
 	array = []
-	for x in range(1,100): 
+	for x in range(1,80): 
 		num = x
-		if (num%3 == 0):
+		if (num%3==0 and num%5==0):
+			num = "FizzBuzz"
+		elif (num%3 == 0):
 			num = "Fizz"
 		elif (num%5 == 0):
 			num = "Buzz"
