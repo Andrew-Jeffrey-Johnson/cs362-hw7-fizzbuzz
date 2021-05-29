ed114310f1f1a57e3ff5966b866ea775ba8164da
johnand4 <63374727+johnand4@users.noreply.github.com>
2021-05-28 17:56:30 -0700

    Wrote test for printing "Buzz"
    This test checks if "Buzz" is printed at element 5

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,8 +5,11 @@ class testCaseFizzBuzz(unittest.TestCase):
 	def test_print_1(self):
 		self.assertEqual(fizzbuzz.fizzbuzz()[0], 1)
 
-	def test_print_100(self):
+	def test_print_fizz(self):
 		self.assertEqual(fizzbuzz.fizzbuzz()[2], "Fizz")
+
+	def test_print_buzz(self):
+		self.assertEqual(fizzbuzz.fizzbuzz()[4], "Buzz")
 
 	if __name__ == '__main__':
 		unittest.main()
