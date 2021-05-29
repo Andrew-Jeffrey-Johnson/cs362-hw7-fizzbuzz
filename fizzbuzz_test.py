42187a214af6f460e631698d99dd5ce1908caa6d
johnand4 <63374727+johnand4@users.noreply.github.com>
2021-05-28 17:46:42 -0700

    Wrote test for printing Fizz
    This test checks if the third element is "Fizz"

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -2,8 +2,11 @@ import unittest
 import fizzbuzz
 
 class testCaseFizzBuzz(unittest.TestCase):
-	def test_print_100(self):
+	def test_print_1(self):
 		self.assertEqual(fizzbuzz.fizzbuzz()[0], 1)
+
+	def test_print_100(self):
+		self.assertEqual(fizzbuzz.fizzbuzz()[2], "Fizz")
 
 	if __name__ == '__main__':
 		unittest.main()
