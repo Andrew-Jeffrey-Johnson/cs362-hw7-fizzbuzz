2ef623fa878b586b02350f82263d0ca4eab0e678
johnand4 <63374727+johnand4@users.noreply.github.com>
2021-05-28 19:05:18 -0700

    Implementing replacing all FizzBuzz
    The function now replaces all multiples of both 3 and 5 in the range 1 through 100 with FizzBuzz.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,7 @@ def fizzbuzz():
 	array = []
 	for x in range(1,101): 
 		num = x
-		if (num%3==0 and num%5==0 and num==15):
+		if (num%3==0 and num%5==0):
 			num = "FizzBuzz"
 		elif (num%3 == 0):
 			num = "Fizz"
