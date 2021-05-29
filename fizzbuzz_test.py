630bb070a0f05586d28c4880365d9e54710a86c2
johnand4 <63374727+johnand4@users.noreply.github.com>
2021-05-28 17:41:14 -0700

    Implemented printing 1
    The function now returns 1 as the first element in the array. The array returns and prints the range from 1 to 100.

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -3,7 +3,7 @@ import fizzbuzz
 
 class testCaseFizzBuzz(unittest.TestCase):
 	def test_print_100(self):
-		self.assertEqual(fizzbuzz.fizzbuzz()[1], 1)
+		self.assertEqual(fizzbuzz.fizzbuzz()[0], 1)
 
 	if __name__ == '__main__':
 		unittest.main()
