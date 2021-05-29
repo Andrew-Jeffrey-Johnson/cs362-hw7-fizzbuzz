fc6833d8e193abdf77557292b8bcb7b790ea51b1
johnand4 <63374727+johnand4@users.noreply.github.com>
2021-05-28 18:00:00 -0700

    Implemented 5th element as "Buzz"
    Element 5 prints "Buzz" rather than 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,11 @@
 def fizzbuzz():
 	array = []
-	for x in range(1,101): 
+	for x in range(1,100): 
 		num = x
 		if (num%3 == 0):
 			num = "Fizz"
+		elif (num%5 == 0):
+			num = "Buzz"
 		array = array + [num]
 	for x in array: print(x)
 	return array
